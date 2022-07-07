95cc06790bb59086f6b9956e207a174c5e796933
zhangtianyuan <tianyuan233.zhang@gmail.com>
2022-07-07 09:37:18 +0800

    config

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,57 +1,86 @@
-export default {
+import { defineConfig } from 'vitepress'
+
+export default defineConfig({
   lang: 'zh',
   title: '前端开发技术笔记',
-  titleTemplate: '分享前端开发技术和 Node.js 开发技术',
-  description: '分享前端开发技术和 Node.js 开发技术',
+  titleTemplate: '记录前端开发技术和 Node.js 开发技术',
+  description: '记录前端开发技术和 Node.js 开发技术',
   markdown: {
     lineNumbers: true,
   },
   lastUpdated: true,
   themeConfig: {
-    // logo: '/icon.png',
+    logo: '/logo.svg',
     siteTitle: false,
-    nav: [
-      { text: '首页', link: '/' },
-      {
-        text: '前端基础',
-        items: [
-          { text: 'HTML', link: '/html' },
-          { text: 'CSS', link: '/css' },
-          { text: 'JavaScript', link: '/javascript' },
-          { text: 'TypeScript', link: '/typescript' },
-        ],
-      },
-      {
-        text: '前端框架',
-        items: [
-          { text: 'Vue', link: '/vue' },
-          { text: 'React', link: '/react' },
-        ],
-      },
-      {
-        text: '前端工程化',
-        link: '/engineering',
-      },
-      {
-        text: '性能优化',
-        link: '/performance',
-      },
-      // {
-      //   text: '浏览器原理',
-      //   link: '/browser',
-      // },
-      // {
-      //   text: '移动端',
-      //   link: '/mobile',
-      // },
-      {
-        text: 'Node.js',
-        link: '/nodejs',
-      },
-    ],
+    nav: nav(),
     socialLinks: [{ icon: 'github', link: 'https://github.com/tianyuan233' }],
     footer: {
       copyright: 'Copyright © 2022-present tianyuan233',
     },
   },
+})
+
+function nav() {
+  return [
+    { text: '首页', link: '/' },
+    { text: '前端基础', link: '/frontend/' },
+    { text: '前端框架', link: '/frameworks/' },
+    { text: '前端工程化', link: '/engineering',},
+    { text: '性能优化', link: '/performance',},
+    { text: 'Node.js', link: '/nodejs',},
+  ]
+}
+
+function sidebarGuide() {
+  return [
+    {
+      text: 'Introduction',
+      collapsible: true,
+      items: [
+        { text: 'What is VitePress?', link: '/guide/what-is-vitepress' },
+      ]
+    },
+    {
+      text: 'Writing',
+      collapsible: true,
+      items: [
+        { text: 'Markdown', link: '/guide/markdown' },
+      ]
+    },
+    {
+      text: 'Theme',
+      collapsible: true,
+      items: [
+        { text: 'Introduction', link: '/guide/theme-introduction' },
+      ]
+    },
+    {
+      text: 'Migrations',
+      collapsible: true,
+      items: [
+        {
+          text: 'Migration from VuePress',
+          link: '/guide/migration-from-vuepress'
+        },
+        {
+          text: 'Migration from VitePress 0.x',
+          link: '/guide/migration-from-vitepress-0'
+        }
+      ]
+    }
+  ]
+}
+
+function sidebarConfig() {
+  return [
+    {
+      text: 'Config',
+      items: [
+        { text: 'Introduction', link: '/config/introduction' },
+        { text: 'App Configs', link: '/config/app-configs' },
+        { text: 'Theme Configs', link: '/config/theme-configs' },
+        { text: 'Frontmatter Configs', link: '/config/frontmatter-configs' }
+      ]
+    }
+  ]
 }
